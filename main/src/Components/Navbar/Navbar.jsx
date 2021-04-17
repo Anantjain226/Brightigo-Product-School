4fbb79cc301f4b473cd8c3043eaa587e1fa436de
Anant Jain <anantjain226@gmail.com>
2021-04-17 22:23:27 +0530

    currentUserDetails fetch + New Mentor Creation

--- main/src/Components/Navbar/Navbar.jsx
+++ main/src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,40 @@
 import React from 'react'
 import styles from './style.module.css'
+import firebase from 'firebase'
+import { useHistory } from 'react-router'
+import {useSelector, useDispatch} from 'react-redux'
+import { openLoginModal, setUserDetails } from '../../Redux/action'
 
 export const Navbar = () => {
+    const history = useHistory()
+    const dispatch= useDispatch()
+    const isAuth = useSelector(state => state.isAuth)
+    const loginModal = useSelector(state => state.loginModal)
+
+    const logoClick = () => {
+        history.push("/")
+    }
+
+    const handleLoginModal= () =>{
+        dispatch(openLoginModal(true))
+    }
+
+    const handleSignOut = () => {
+        firebase.auth().signOut()
+        dispatch(setUserDetails(""))
+        dispatch(openLoginModal(false))
+    }
+
     return (
         <section className = {styles.navBar} >
-            <img src = "https://producthyre.com/images/logo.png" />
+            <img src = "https://producthyre.com/images/logo.png" onClick = {logoClick}/>
             <div>
                 <div>Post Jobs</div>
                 <div>Hire Product Managers!</div>
             </div>
-            <button>Sign In</button>
-            <button>Sign Out</button>
+            {
+                !isAuth? <button onClick = {handleLoginModal} >Sign In</button> : <button onClick = {handleSignOut} >Sign Out</button>
+            }
         </section>
     )
 }
